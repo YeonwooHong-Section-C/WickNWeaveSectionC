3c4dd2324f205b999424b9b1ec57627d3f4abf48
SheleezaLeslieSectionC <156844756+SheleezaLeslieSectionC@users.noreply.github.com>
2024-04-07 11:34:14 -0400

    save cart array in local storage

--- scripts/product_description.js
+++ scripts/product_description.js
@@ -3,37 +3,43 @@ let main = document.querySelector('.main-img');
 let productName = document.querySelector('.product-name');
 let price = document.querySelector('.price');
 let info = document.querySelector('.info');
-let addCart = document.querySelector('.add')
+let addCart = document.querySelector('.add');
 
+let userCart = JSON.parse(localStorage.getItem('cart'));
+console.log(userCart);
 let cart = [];
 
 const productsArray = [
     {
-        name: "Product 1",
-        price: 150,
-        imageUrl: 'https://placedog.net/574/584?id=57',
+        prod_name: "Product 1",
+        prod_price: 150,
+        prod_img: 'https://placedog.net/574/584?id=57',
         description: 'product description for 1',
+        prod_quantity: null,
         id: 1
     },
     {
-        name: "Product 2",
-        price: 20,
-        imageUrl: 'https://placedog.net/496/476?id=122',
+        prod_name: "Product 2",
+        prod_price: 20,
+        prod_img: 'https://placedog.net/496/476?id=122',
         description: 'product description for 2',
+        prod_quantity: null,
         id: 2
     },
     {
-        name: "Product 3",
-        price: 30,
-        imageUrl: 'https://placedog.net/541/553?id=119',
+        prod_name: "Product 3",
+        prod_price: 30,
+        prod_img: 'https://placedog.net/541/553?id=119',
         description: 'product description for 3',
+        prod_quantity: null,
         id: 3
     },
     {
-        name: "Product 4",
-        price: 10,
-        imageUrl: 'https://placedog.net/807/669?id=142',
+        prod_name: "Product 4",
+        prod_price: 10,
+        prod_img: 'https://placedog.net/807/669?id=142',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet consectetur adipiscing elit ut aliquam purus sit amet. Urna id volutpat lacus laoreet non curabitur gravida. Id diam vel quam elementum. Vulputate eu scelerisque felis imperdiet proin fermentum leo vel. Curabitur vitae nunc sed velit dignissim sodales ut eu. Egestas pretium aenean pharetra magna ac placerat vestibulum. Mi proin sed libero enim sed faucibus turpis. Arcu non sodales neque sodales ut etiam sit. bortis feugiat. Nisl condimentum id venenatis a. Vel pretium lectus quam id leo in vitae turpis massa.',
+        prod_quantity: null,
         id: 4
     }
 ]
@@ -49,32 +55,45 @@ images.forEach(image => {
 
         // update data on page based on selected product
         productsArray.forEach(product => {
-            if (main.src == product.imageUrl) {
-                productName.textContent = product.name;
-                price.textContent = product.price;
+            if (main.src == product.prod_img) {
+                productName.textContent = product.prod_name;
+                price.textContent = product.prod_price;
                 main.id = product.id;
                 info.textContent = product.description;
                 addCart.id = product.id;
-            }           
-        });    
-    });   
+            }
+        });
+    });
 });
 
 // adding items to cart
 
-addCart.addEventListener('click', function() {
+addCart.addEventListener('click', function () {
+    let quantity = document.querySelector('.quantity').value;
+    console.log(quantity);
     productsArray.forEach(product => {
-        if (addCart.id == product.id){
-            cart.push([product.name, product.price, product.id]);
+        if (addCart.id == product.id) {
+            product.prod_quantity = quantity;
+            checkCopy(cart, product);
+            localStorage.setItem('cart', JSON.stringify(cart));
+            // cart.push([product.name, product.price, product.id]);
         }
     })
     console.log(cart);
+
 });
 
-// function to make cart array
-// function to remove item from cart
-// function to accomodate quantity
-// add image 
-// add quantity
+//function to overwrite value in array if duplicate is present
+function checkCopy(array, item, key='name'){
+    const index = array.findIndex(e => e[key] === item[key]);
+    if (index >= 0) {
+        array[index] = item;
+      }
+      else {
+        array.push(item);
+      }
+};
+
+
 
 
